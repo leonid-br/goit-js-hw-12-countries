536c7462164684e0d2107be8ab348244d30c5b3e
unknown <74738757+leonid-br@users.noreply.github.com>
2021-11-19 00:03:41 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -11,60 +11,58 @@ import countrysMarcup from './templates/countrys';
 import countryMarcup from './templates/country';
 
 // Import Notification
-import "@pnotify/core/dist/BrightTheme";
-import "@pnotify/core/dist/Material";
-import { alert, error, defaultModules } from "@pnotify/core/dist/PNotify";
+import '@pnotify/core/dist/BrightTheme';
+import '@pnotify/core/dist/Material';
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify';
 // import * as PNotify from "@pnotify/core/dist/PNotify";
 
 // Import Variables
-import refs from './js/refs'
+import refs from './js/refs';
 
 // Event Handlers
-refs.inputEl.addEventListener('input', debounce(onSearch,1000))
-
+refs.inputEl.addEventListener('input', debounce(onSearch, 1000));
 
 // Functions
 function onSearch(e) {
   const country = refs.inputEl.value.trim();
   e.preventDefault();
-  
-  fetchCountries(country)
-  .then(country=>{
-    refs.countrysMarcupEL.innerHTML=''
-    searchCountry(country)
-  })
+
+  fetchCountries(country).then(country => {
+    if (country.status === 404) {
+      error({
+        title: 'Oh No!',
+        text: 'This country does not exist',
+        delay: 2,
+      });
+    }
+    refs.countrysMarcupEL.innerHTML = '';
+    searchCountry(country);
+  });
 }
 
 function searchCountry(country) {
   const marcupCountry = countryMarcup(country);
-  const marcupCountrys = countrysMarcup(country)
+  const marcupCountrys = countrysMarcup(country);
 
-  if (country.length===1){           
-    refs.countrysMarcupEL.insertAdjacentHTML('beforeend', marcupCountry)
-    return}
-  if (country.length>10){alert({
-    title: 'Notification',
-    text: 'Enter more letters',
-    delay: 2
-  });}
-  if (country.length<=10){ 
-    refs.countrysMarcupEL.insertAdjacentHTML('beforeend',marcupCountrys)   
-        }
-  if (refs.inputEl.value!==''&& country.length===0){error({
-    title: 'Oh No!',
-    text: 'This country does not exist',
-    delay: 2
-  })
-    ;} 
+  if (country.length === 1) {
+    refs.countrysMarcupEL.insertAdjacentHTML('beforeend', marcupCountry);
+    return;
+  }
+  if (country.length > 10) {
+    alert({
+      title: 'Notification',
+      text: 'Enter more letters',
+      delay: 2,
+    });
+  }
+  if (country.length <= 10) {
+    refs.countrysMarcupEL.insertAdjacentHTML('beforeend', marcupCountrys);
+  }
+  if (refs.inputEl.value !== '' && country.length === 0) {
+    error({
+      title: 'Oh No!',
+      text: 'This country does not exist',
+      delay: 2,
+    });
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,19 +1,14 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
-const CEARCH = '?fields=name;population;flag;languages;capital'
-
+const BASE_URL = 'https://restcountries.com/v2/name/';
+const CEARCH = '?fields=name,population,flag,languages,capital';
 
 export default function fetchCountries(searchCountry) {
   const url = `${BASE_URL}${searchCountry}${CEARCH}`;
 
-    return fetch(url)
-      .then(response => {
-        if(response.ok){
-          return response.json()}
-        else{return []}
-        } )
-   
-} 
-
-
-
-
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return [];
+    }
+  });
+}
